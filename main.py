5299294172af4fa4b3d3fd8a7c19851e303480d8
iamthegeek19 <callmethegeek19@gmail.com>
2024-04-29 15:16:08 +0200

    model working with 5 images in a folder

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+from matplotlib.pyplot import imread
+
+app = FastAPI()
+
+class_names = ['Bird-drop', 'Clean', 'Dusty', 'Electrical-damage', 'Physical-Damage']
+
+def load_and_prep_image(image):
+  img = tf.image.resize(image, [224,224])
+  return img
+
+data = [
+   {
+      'name': 'clean',
+      'img_path': './images/bird.jpg'
+   },
+   {
+      'name': 'bird',
+      'img_path': './images/clean.jpg'
+   },
+   {
+      'name': 'dust',
+      'img_path': './images/dust.jpg'
+   },
+   {
+      'name': 'electrical',
+      'img_path': './images/electrical.jpg'
+   },
+   {
+      'name': 'physical',
+      'img_path': './images/physical.jpg'
+   }
+]
+
+results = []
+
+@app.get('/predict/')
+def hello():
+    model = load_model('native_model')
+    for i in range(len(data)):
+        img = imread(data[i]['img_path'])
+        pred_img = load_and_prep_image(img)
+        pred_prob = model.predict(tf.expand_dims(pred_img, axis=0))
+        res = tf.argmax(pred_prob, axis=1).numpy()[0]
+        results.append({
+           data[i]['name']: class_names[res]
+        })
+    
+    print(results)
+    return {'data': results}
+    
